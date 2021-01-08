5b39b6f7d191a256e154a15a06584553e55ac821
awilson12 <apfeifer@email.arizona.edu>
2021-01-07 17:22:24 -0700

    infrastructure for rate and probability steps

---
+++ defining_rates.R
@@ -0,0 +1,41 @@
+#non-zero rates for model A
+
+lambda.1.2.A
+lambda.1.3.A
+
+lambda.2.1.A
+lambda.2.3.A
+lambda.2.4.A
+
+#non-zero rates for model B
+
+lambda.1.2.B
+lambda.2.3.B
+lambda.1.4.B
+
+lambda.2.1.B
+lambda.2.4.B
+
+lambda.3.1.B
+lambda.3.4.B
+lambda.3.5.B
+
+#non-zero rates for model C
+
+lambda.1.3.C
+lambda.1.4.C
+lambda.1.5.C
+
+lambda.2.3.C
+lambda.2.4.C
+lambda.2.5.C
+
+lambda.3.1.C
+lambda.3.2.C
+lambda.3.5.C
+
+lambda.4.1.C
+lambda.4.2.C
+lambda.4.5.C
+lambda.4.6.C
+
