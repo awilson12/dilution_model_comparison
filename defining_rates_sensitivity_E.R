5a6c2b6aba613eb8bcc1610decf3817c4814f3f6
amwilson12 <47330626+awilson12@users.noreply.github.com>
2021-09-02 11:44:56 -0700

    updates using model D since model C changed

--- defining_rates_sensitivity_E.R
+++ defining_rates_sensitivity_E.R
@@ -1,23 +1,22 @@
 
 #non-zero rates for model C
+lambda.1.3.D<-(1/SA.smallfomite)*S.H*TE.FH*A.hand*H.sm.right*timestep
+lambda.1.4.D<-(1/SA.smallfomite)*S.H*TE.FH*A.hand*H.sm.left*timestep
+lambda.1.5.D<-k.fome*timestep
 
-lambda.1.3.C<-(1/SA.smallfomite)*S.H*TE.FH*A.hand*H.sm.right*timestep
-lambda.1.4.C<-(1/SA.smallfomite)*S.H*TE.FH*A.hand*H.sm.left*timestep
-lambda.1.5.C<-k.fome*timestep
+lambda.2.3.D<-(1/SA.largefomite)*S.H*TE.FH*A.hand*H.lg.right*timestep
+lambda.2.4.D<-(1/SA.largefomite)*S.H*TE.FH*A.hand*H.lg.left*timestep
+lambda.2.5.D<-k.fome*timestep
 
-lambda.2.3.C<-(1/SA.largefomite)*S.H*TE.FH*A.hand*H.lg.right*timestep
-lambda.2.4.C<-(1/SA.largefomite)*S.H*TE.FH*A.hand*H.lg.left*timestep
-lambda.2.5.C<-k.fome*timestep
-
-lambda.3.1.C<-S.H*TE.HF*H.sm.right*timestep
-lambda.3.2.C<-S.H*TE.HF*H.lg.right*timestep
-lambda.3.5.C<-k.skin*timestep
+lambda.3.1.D<-S.H*TE.HF*H.sm.right*timestep
+lambda.3.2.D<-S.H*TE.HF*H.lg.right*timestep
+lambda.3.5.D<-k.skin*timestep
 #lambda.3.6.C<-S.Face*TE.HM*H.face*0.25*timestep
-lambda.3.6.C<-S.Face*TE.HM*H.face*0.5*timestep
+lambda.3.6.D<-S.Face*TE.HM*H.face*0.5*timestep
 
-lambda.4.1.C<-S.H*TE.HF*H.sm.left*timestep
-lambda.4.2.C<-S.H*TE.HF*H.lg.left*timestep
-lambda.4.5.C<-k.skin*timestep
+lambda.4.1.D<-S.H*TE.HF*H.sm.left*timestep
+lambda.4.2.D<-S.H*TE.HF*H.lg.left*timestep
+lambda.4.5.D<-k.skin*timestep
 #lambda.4.6.C<-S.Face*TE.HM*H.face*0.75*timestep
-lambda.4.6.C<-S.Face*TE.HM*H.face*0.5*timestep
+lambda.4.6.D<-S.Face*TE.HM*H.face*0.5*timestep
 
